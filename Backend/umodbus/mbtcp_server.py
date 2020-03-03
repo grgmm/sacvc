93e31d0ca73b4f368f8678d15421215e818640cc
grgmm <miguelazomm@gmail.com>
2020-03-02 22:43:48 -0400

    f_adquisicion_4_5 cliente umodbus funcionando

--- Backend/umodbus/mbtcp_server.py
+++ Backend/umodbus/mbtcp_server.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# scripts/examples/simple_data_store.py
+# scripts/examples/simple_tcp_server.py SERVIDOR DE DATOS ESCLAVO MODBUS
 import logging
 from socketserver import TCPServer
 from collections import defaultdict
@@ -11,30 +11,32 @@ from umodbus.utils import log_to_stream
 # Add stream handler to logger 'uModbus'.
 log_to_stream(level=logging.DEBUG)
 
-# A very simple data store which maps addresses against their values.
+# A very simple data store which maps addresss against their values.
 data_store = defaultdict(int)
 
 # Enable values to be signed (default is False).
 conf.SIGNED_VALUES = True
 
 TCPServer.allow_reuse_address = True
-app = get_server(TCPServer, ('localhost', 1502), RequestHandler)
+app = get_server(TCPServer, ('localhost', 502), RequestHandler)
 
 
-@app.route(slave_ids=[1], function_codes=[1, 2], addresses=list(range(0, 10)))
+@app.route(slave_ids=[1], function_codes=[3, 4], addresses=list(range(0, 10)))
 def read_data_store(slave_id, function_code, address):
     """" Return value of address. """
+    
     return data_store[address]
 
 
-@app.route(slave_ids=[1], function_codes=[5, 15], addresses=list(range(0, 10)))
+@app.route(slave_ids=[1], function_codes=[6, 16], addresses=list(range(0, 10)))
 def write_data_store(slave_id, function_code, address, value):
     """" Set value for address. """
-    data_store[address] = value
+    data_store[address] = 1
+
 
 if __name__ == '__main__':
     try:
-        app.serve_forever()
+    	app.serve_forever()
     finally:
         app.shutdown()
         app.server_close()
