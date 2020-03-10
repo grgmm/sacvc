c833fc2f43e7d1402dacda01aaead733ee7ea8fb
grgmm <miguelazomm@gmail.com>
2020-03-10 15:41:14 -0400

    f_adquisicion_5_1_5

---
+++ Backend/Simulador_v1.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+import random 
+import time
+import json
+import socket
+import sys
+from umodbus import conf
+from umodbus.client import tcp
+
+def aleatorio():
+  n=100
+  conf.SIGNED_VALUES = True
+  print('\n' '\n'  "         PRUEBA MODBUS DESARROLLADA POR: Ing Miguel Moreno")
+  print('\n' '\n' "   Dirección IP del Esclavo Modbus: 192.168.43.143")
+  print('\n' '\n' "Puerto del Esclavo Modbus: 5002")
+  print('\n' '\n' "Id del Esclavo 1 Modbus 11")
+  input('\n' '\n' "Dirección del registro en el Esclavo Modbus 101 ")
+#Requeired_Value = int(input('\n' '\n' "Valor Requerido "))
+  #sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  #sock.connect(('192.168.43.214', 5002))
+  i=0
+  while i<n:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(('192.168.43.143', 5002))
+    Current_Value=random.randint(0,1000)
+    message = tcp.write_single_register(slave_id = 11, address = 101, value = Current_Value)
+    response = tcp.send_message(message, sock)
+    print(response)
+    sock.close()
+    time.sleep(2)
+    i+=1	
+  
+
+  return
+
+
+aleatorios=aleatorio()
