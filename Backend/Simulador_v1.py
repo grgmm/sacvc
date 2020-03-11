b52efdf132efa7c544fb250629348ad9aa663d6f
grgmm <miguelazomm@gmail.com>
2020-03-11 10:16:42 -0400

    f_adquisicion_5_1_7

--- Backend/Simulador_v1.py
+++ Backend/Simulador_v1.py
@@ -14,20 +14,23 @@ def aleatorio():
   print('\n' '\n' "   Dirección IP del Esclavo Modbus: 192.168.43.143")
   print('\n' '\n' "Puerto del Esclavo Modbus: 5002")
   print('\n' '\n' "Id del Esclavo 1 Modbus 11")
-  input('\n' '\n' "Dirección del registro en el Esclavo Modbus 101 ")
+  input('\n' '\n' "Dirección los registro en el Esclavo Modbus 101 y 201 ")
 #Requeired_Value = int(input('\n' '\n' "Valor Requerido "))
   #sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   #sock.connect(('192.168.43.214', 5002))
   i=0
   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   sock.connect(('192.168.43.143', 5002))
-  while i<n:
-    
-    Current_Value=random.randint(0,1000)
-    message = tcp.write_single_register(slave_id = 11, address = 101, value = Current_Value)
-    response = tcp.send_message(message, sock)
-    print(response)
-    time.sleep(2)
+  while i<n:    
+    Current_Value1=random.randint(0,1000)
+    Current_Value2=random.randint(0,1000)
+    message1 = tcp.write_single_register(slave_id = 11, address = 101, value = Current_Value1)
+    message2 = tcp.write_single_register(slave_id = 11, address = 201, value = Current_Value2)
+    response1 = tcp.send_message(message1, sock)
+    response2 = tcp.send_message(message2, sock)
+    print(response1)
+    print(response2)
+    time.sleep(1)
     i+=1
   sock.close()
   
