31d822c63827b0c455086ef64c0bbde7d69a596e
grgmm <miguelazomm@gmail.com>
2020-03-11 15:31:43 -0400

    f_adquisicion_5_1_10

--- Backend/Llenar_registros_v1.py
+++ Backend/Llenar_registros_v1.py
@@ -8,7 +8,7 @@ from umodbus import conf
 from umodbus.client import tcp
 
 def escribir_registros():
-  n=100
+  n=10
   conf.SIGNED_VALUES = True
   print('\n' '\n'  "         PRUEBA MODBUS DESARROLLADA POR: Ing Miguel Moreno")
   print('\n' '\n' "   Dirección IP del Esclavo Modbus: 192.168.43.143")
@@ -28,23 +28,28 @@ def escribir_registros():
     message1 = tcp.write_multiple_registers(slave_id = 11, starting_address = 101, values = [Current_Value1, Current_Value2, Current_Value3, Current_Value4])
     escribir = tcp.send_message(message1, sock)
  
-    
-
     #Leer
 
     message2 = tcp.read_holding_registers(slave_id =11, starting_address = 101, quantity=4)
     leer = tcp.send_message(message2, sock)
     print(leer)
-    time.sleep(1)
 
 
+
+    with open ('datos.json','w') as file: #abre un archivo json para escrtitura
+
+     j=0
+     while j < 4:
+
+       file.write(json.dumps({"id_Tag": 9,"Timestamp": str(datetime.now()),"Pv": leer[j]})+ "\n")
+       time.sleep(1)
+       j+=1
 
 
 
 
     i+=1
-  sock.close()
-  
+  sock.close()  
 
   return
 
