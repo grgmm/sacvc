e6a97f3b1827d2be78ee791f1fe9e0e1a1e98100
grgmm <miguelazomm@gmail.com>
2020-10-18 11:48:12 -0400

    Despliegue Heroku v3

--- sacvc/settings.py
+++ sacvc/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4nrii@bdhlgv%w&#qikkht!cf8+)fe&wfs7cu8=i*9@rfbng3q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com]
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
