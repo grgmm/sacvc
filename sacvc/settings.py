b75ee8ba99c548a1c7c0498fcb59cb83a5bf237c
grgmm <miguelazomm@gmail.com>
2020-10-18 11:51:26 -0400

    Despliegue Heroku v4

--- sacvc/settings.py
+++ sacvc/settings.py
@@ -138,4 +138,4 @@ MEDIA_URL= '/configuraciones/'
 
 #para Heroku:
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASE['default'].update(db_from_env
+DATABASE['default'].update(db_from_env)
