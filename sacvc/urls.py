24c6b88b0415c04d1f87a8db12eaf79dbc05a1cf
Miguel Angel Perez <miguelazomm@gmail.com>
2021-04-20 13:12:38 +0200

    hk cuena nueva vx

--- sacvc/urls.py
+++ sacvc/urls.py
@@ -14,21 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+
+
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
 
-
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('acq/', include('acq.urls',namespace="uacq")),
-   
+    #path('acq/', include('acq.urls',namespace="uacq")),
+    path('sacvc/', include('acq.urls',namespace="uacq")),
 
-   ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+   ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
