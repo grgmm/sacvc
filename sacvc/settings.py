84c6b7e9e3183bcbb142f52b1b7e47346a794510
Miguel Angel Perez <miguelazomm@gmail.com>
2021-04-20 13:35:09 +0200

    hk cuena nueva vx6

--- sacvc/settings.py
+++ sacvc/settings.py
@@ -152,8 +152,8 @@ NUMBER_GROUPING = (3, 2, 0)
 #STATIC_URL = '/static/'
 
 #STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+    #os.path.join(BASE_DIR, 'static'),
+#)
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_DATA = os.path.join(BASE_DIR, 'media/Data')
 MEDIA_URL= '/configuraciones/'
