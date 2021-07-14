c2e741a94c9993fa9264c974681e4b2b7c9341ed
jopegoco <jcotelo@gmail.com>
2021-07-14 12:30:52 +0200

    Se optimizo leer_json.js

--- acq/management/commands/acq.py
+++ acq/management/commands/acq.py
@@ -18,21 +18,23 @@ class Command(BaseCommand):
     help = 'help'
 
     def handle(self, *args, **kwargs):
-        conf.SIGNED_VALUES = True  # No estoy seguro de su utilidad me lo copié del ejemplo.
+        # No estoy seguro de su utilidad me lo copié del ejemplo.
+        conf.SIGNED_VALUES = True
         slaveid = 11  # ide del esclavo 0-247 segun Modbus doc
-        slaveport = 5002  # puertos validos por encima de 1024 en sistemas Linux Android Unix.
+        # puertos validos por encima de 1024 en sistemas Linux Android Unix.
+        slaveport = 5002
 
+        slaveip = '192.168.1.40'  # ip del esclavo para modbus TCP
 
-       slaveip = '192.168.1.40'  # ip del esclavo para modbus TCP
-
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # declara la conexión
+        # declara la conexión
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         try:
             sock.connect((slaveip, slaveport))  # realiza la conexión
 
         except:
-             print("Sin conexión...reintentando", sys.exc_info()[0])
-             pass
+            print("Sin conexión...reintentando", sys.exc_info()[0])
+            pass
 
         print('Adquisción de Data Modbus Activa (Running)')
 
@@ -49,69 +51,78 @@ class Command(BaseCommand):
         k = 0
         TKS = {}
         Parametro_tk = ''
-        Pv0 = 0  # simula el valor medido de un transmisor (registro menos significativo) del Float IEE754
+        # simula el valor medido de un transmisor (registro menos significativo) del Float IEE754
+        Pv0 = 0
         Pv1 = 0
 
         while i <= n:
             if not Tk.objects.exists():
-                print('NO HAY DATOS PARA ENCUESTAR SALIENDO DEL ADQUISIDOR ###################')
+                print(
+                    'NO HAY DATOS PARA ENCUESTAR SALIENDO DEL ADQUISIDOR ###################')
                 exit()  # SALIR DEL PROGRAMA SI NO HAY TANQUES QUE ENCUESTAR
             else:
-              for tk in Tk.objects.iterator():  # ITERNDO EN TANQUES EXISTENTES.
-                Data_Cruda = {'Data_Cruda':[] }
-                for tag in Tag.objects.filter(id_Tk=tk.pk).iterator():  # RECORRIENDO LOS TAGS DE CADA TANQUE
-                    # Pv0=random.randint(16384,32765)    #simula el valor medido de un transmisor (registro menos significativo) del Float IEE754
-                    # Pv1=random.randint(16000,17900)    #simula el valor medido de un transmisor (registro mas significativo) del Float IEEE754
-                    idtag = tag.pk  # Del mdelo Tag
-                    tag_addres = int(tag.direccion_campo) - 1  # Del mdelo Tag
-                    Current_Value = [idtag, Pv0, Pv1]  # DATA PARA TRASNFERIR
+                for tk in Tk.objects.iterator():  # ITERNDO EN TANQUES EXISTENTES.
+                    Data_Cruda = {'Data_Cruda': []}
+                    # RECORRIENDO LOS TAGS DE CADA TANQUE
+                    for tag in Tag.objects.filter(id_Tk=tk.pk).iterator():
+                        # Pv0=random.randint(16384,32765)    #simula el valor medido de un transmisor (registro menos significativo) del Float IEE754
+                        # Pv1=random.randint(16000,17900)    #simula el valor medido de un transmisor (registro mas significativo) del Float IEEE754
+                        idtag = tag.pk  # Del mdelo Tag
+                        tag_addres = int(tag.direccion_campo) - \
+                            1  # Del mdelo Tag
+                        # DATA PARA TRASNFERIR
+                        Current_Value = [idtag, Pv0, Pv1]
 
-                   # ESCRIBIR
-                    # message1 = tcp.write_multiple_registers(slave_id = slaveid, starting_address = tag_addres, values = list(Current_Value))
-                    # Se construye el msj de escritura en el esclavo(SIMULACION)
-                    # escribir = tcp.send_message(message1, sock) #Se envia comando de escritura con el msj al esclavo en el socket abierto.
+                       # ESCRIBIR
+                        # message1 = tcp.write_multiple_registers(slave_id = slaveid, starting_address = tag_addres, values = list(Current_Value))
+                        # Se construye el msj de escritura en el esclavo(SIMULACION)
+                        # escribir = tcp.send_message(message1, sock) #Se envia comando de escritura con el msj al esclavo en el socket abierto.
 
-                    # Leer
-                    message2 = tcp.read_holding_registers(slave_id=slaveid, starting_address=tag_addres, quantity=2)
-                    # Construcción del msj de lectura desde el esclavo partiendo de la dirección mb configurada en el modelo Tag
-                    leer = tcp.send_message(message2, sock)
-                    # Se envia comando de lectura en el esclavo en el socket abierto.
-                    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-7]
-                    tag_instance = Tag.objects.get(pk=idtag)  # id extraido del paquete transferido
+                        # Leer
+                        message2 = tcp.read_holding_registers(
+                            slave_id=slaveid, starting_address=tag_addres, quantity=2)
+                        # Construcción del msj de lectura desde el esclavo partiendo de la dirección mb configurada en el modelo Tag
+                        leer = tcp.send_message(message2, sock)
+                        # Se envia comando de lectura en el esclavo en el socket abierto.
+                        timestamp = datetime.now().strftime(
+                            '%Y-%m-%d %H:%M:%S.%f')[:-7]
+                        # id extraido del paquete transferido
+                        tag_instance = Tag.objects.get(pk=idtag)
 
-                    Data_Cruda_Temp = {'IDTAG':idtag,
-                    'REGISTRO_1':leer[0],
-                    'REGISTRO_2':leer[1],
-                    'TIMESTAMP': timestamp,
-                    'INDEXADO':False
-                    }
+                        Data_Cruda_Temp = {'IDTAG': idtag,
+                                           'REGISTRO_1': leer[0],
+                                           'REGISTRO_2': leer[1],
+                                           'TIMESTAMP': timestamp,
+                                           'INDEXADO': False
+                                           }
 
-                    Data_Cruda['Data_Cruda'].append(Data_Cruda_Temp)
-                    # print(Data_Cruda)
-                    # datacruda=json.dumps(Data_Cruda)
+                        Data_Cruda['Data_Cruda'].append(Data_Cruda_Temp)
+                        # print(Data_Cruda)
+                        # datacruda=json.dumps(Data_Cruda)
 
-                # tk = {"IDTK":tk_instance.pk,IDTAG":str(tag_instance.pk),"INSTALACION":tk_instance.id_patioTanque.Nombre, "TIMESTAMP":timestamp,"PV0":leer[1],"PV1":leer[2], "PV_FLOAT":0.000, "UNIDAD":(Analogico_instance.Unidad),"PARAMETRO_TK":tag_instance.etiqueta1, "INDEXADO": 0}
+                    # tk = {"IDTK":tk_instance.pk,IDTAG":str(tag_instance.pk),"INSTALACION":tk_instance.id_patioTanque.Nombre, "TIMESTAMP":timestamp,"PV0":leer[1],"PV1":leer[2], "PV_FLOAT":0.000, "UNIDAD":(Analogico_instance.Unidad),"PARAMETRO_TK":tag_instance.etiqueta1, "INDEXADO": 0}
 
-                # print(Datos_Actuales) PARA DEBUGGER
+                    # print(Datos_Actuales) PARA DEBUGGER
 
-                try:
-                    with fs.open(ruta_Data + '/Buffer_Data_Cruda.json', mode='w') as file1:
-                # with open ('/home/morenomx/solucionesweb/sacvc/valoresbasicos.json','w') as file1: #abre un archivo json (cambiar por ruta simbólica)
+                    try:
+                        with fs.open(ruta_Data + '/Buffer_Data_Cruda.json', mode='w') as file1:
+                            # with open ('/home/morenomx/solucionesweb/sacvc/valoresbasicos.json','w') as file1: #abre un archivo json (cambiar por ruta simbólica)
 
-                            file1.write(json.dumps(Data_Cruda))  # Data en cache
-                except:
-                     print("Error inesperado:", sys.exc_info()[0])
+                            # Data en cache
+                            file1.write(json.dumps(Data_Cruda))
+                    except:
+                        print("Error inesperado:", sys.exc_info()[0])
 
-                # try:
+                    # try:
 
-                    # with fs.open(ruta_Data+'/Valores_Tk.json', mode= 'w') as file2:
+                        # with fs.open(ruta_Data+'/Valores_Tk.json', mode= 'w') as file2:
 
-                            # file2.write(json.dumps(Data_Cruda)) #Data en cache
+                        # file2.write(json.dumps(Data_Cruda)) #Data en cache
 
-                # tk.current_data = Data_Cruda #A Base de Datos
+                    # tk.current_data = Data_Cruda #A Base de Datos
 
-                # tk.save()
-                # except:
-                # print("Error inesperado:", sys.exc_info()[0])
+                    # tk.save()
+                    # except:
+                    # print("Error inesperado:", sys.exc_info()[0])
 
         sock.close()  # cierra la conexión
--- static/js/leer_json.js
+++ static/js/leer_json.js
@@ -1,11 +1,9 @@
-flag_id = false; //Codigo que sera ejecutado solo al cargar la pagina
+flag_configurar = false; //Codigo que sera ejecutado solo al cargar o refrescar la pagina
 ntanq = 0;//numero de tanques
-datos_anterior = null; //Copia de data json de la ultima actualizacion de la vista
-flag_blink = false; //flag para parpadear alarmas
-flag_lt_critica = []; //alarmas criticas tomadas de json
-flag_lt_urgente = [];//alarmas urgentes tomadas de json
-//flag_lt_normal = [];
-
+datos_anterior = null; //Copia de json de ultimo refresh
+//flag_lt_critica = []; //flag para on/off cuando la alarma esta activa
+//flag_lt_urgente = [];//flag para on/off cuando la alarma esta activa
+flag_alarma=false;
 
 function mi_funcion() {
 	var refresca = $.ajax({
@@ -13,16 +11,17 @@ function mi_funcion() {
 		dataType: 'json', //indicamos que es de tipo json
 		type: 'get'
 	}).done(function (datos) {
-		configurar(datos);
+		if (!flag_configurar) {
+			configurar(datos);
+		}
 		if (JSON.stringify(datos) != JSON.stringify(datos_anterior)) {
 			//solo se ejecuta si el json cambia
 			actualizar_vista(datos);
-			manejar_alarmas(datos);
 		}
-
+		manejar_alarmas(datos);
 	})
 	function configurar(datos) {
-		if (!flag_id) {
+		if (!flag_configurar) {
 			//Este codigo solo se ejecutara al cargar o refrescar la pagina
 			//obtengo el numero de tanques
 			for (var tk in datos) {
@@ -51,19 +50,19 @@ function mi_funcion() {
 						mi_id.setAttributeNode(att);
 
 						//genero los arrys de alarma	
-						flag_lt_critica[contador] = false;
-						flag_lt_urgente[contador] = false;
-						//flag_lt_normal[contador] = false;
+						//flag_lt_critica[contador] = false;
+						//flag_lt_urgente[contador] = false;
 					}
 				}
 			}
 			//Cambio flag para evitar ejecucion de codigo innecesario
-			flag_id = true;
+			flag_configurar = true;
 			console.log("selectores id crearos");
 		}
 	}
 
 	function actualizar_vista(datos) {
+		//solo se ejecuta si el json cambia
 		var t = 1;//EMPIEZO POR LA CELDA1 (TANQUE1)
 		var valormaximo;//valor del span
 		var lt; //valor del nivel actual
@@ -85,7 +84,6 @@ function mi_funcion() {
 			nivel = document.getElementById("barra" + tq); //nivel en UI
 			nivel.style.width = lt_porcentaje + "%"; //barra de progreso
 			$("#nivel" + tq).text(lt_porcentaje + "%");//Valor numerico del nivel
-			console.log(lt_porcentaje);
 			t++ //paso a la celda siguiente (tanque)
 			datos_anterior = datos;//Evita actualizar datos innecesariamente
 		}
@@ -93,12 +91,9 @@ function mi_funcion() {
 	}
 
 	function manejar_alarmas(datos) {
-
+		//solo se ejecuta si el json cambia
 		var t = 1;//EMPIEZO POR LA CELDA1 (TANQUE1)
 		//todo en unidades de ingenieria
-		var valormaximo//valor del valormaximo
-		var valorminimo;
-		//var lt_normal;
 		var lt_urgente;
 		var lt_critica;
 		var idtk;//nro del tanque json
@@ -108,47 +103,47 @@ function mi_funcion() {
 			idtk = tk.toString(); //tomo el id del tanque, uno por celda
 			tq = t.toString();
 			lt = datos[idtk]["LT"];
-			valorminimo = datos[idtk]["VALORMINIMO"];
-			valormaximo = datos[idtk]["VALORMAXIMO"];
 			//lt_normal = datos[idtk]["LT_NORMAL"];
 			lt_urgente = datos[idtk]["LT_URGENTE"];
 			lt_critica = datos[idtk]["LT_CRITICA"];
+
 			//Calculo de alarmas
-
-
 			if (lt_critica) {
-				flag_lt_critica[t] = !flag_lt_critica[t];
-				$("#barra" + tq).css('background', 'red');
-				$("#nivel" + tq).css('color', 'black');
-				if (flag_lt_critica[t]) {
+				//flag_lt_critica[t] = !flag_lt_critica[t];
+				$("#barra" + tq).css('background', 'red');//color de barra
+				$("#nivel" + tq).css('background', 'red');//color de fondo de la fuente
+				$("#nivel" + tq).css('color', 'black');//color de fuente
+				//if (flag_lt_critica[t]) {
+				if (flag_alarma) {
+					//on
 					$("#nivel" + tq).css('background', 'red');
 				} else {
+					//off
 					$("#nivel" + tq).css('background', 'none');
 				}
-			}
-			if (lt_urgente) {
-				//nivel bajo
-				flag_lt_urgente[t] = !flag_lt_urgente[t];
-				$("#barra" + tq).css('background', 'yellow');
-				$("#nivel" + tq).css('background', 'yellow');
-				$("#nivel" + tq).css('color', 'black');
-				if (flag_lt_urgente[t]) {
-					$("#nivel" + tq).css('background', 'yellow');
+			} else {
+				if (lt_urgente) {
+					//flag_lt_urgente[t] = !flag_lt_urgente[t];
+					$("#barra" + tq).css('background', 'yellow');//color de barra
+					$("#nivel" + tq).css('background', 'yellow');//color de fondo de la fuente
+					$("#nivel" + tq).css('color', 'black');//color de fuente
+					//if (flag_lt_urgente[t]) {
+					if (flag_alarma) {
+						$("#nivel" + tq).css('background', 'yellow');
+					} else {
+						$("#nivel" + tq).css('background', 'none');
+					}
 				} else {
-					$("#nivel" + tq).css('background', 'none');
+					//flag_lt_critica[t] = false;
+					//flag_lt_urgente[t] = false;
+					$("#barra" + tq).css('background', 'black');
+					$("#nivel" + tq).css('color', 'green');
+					$("#nivel" + tq).css('background', 'black');
 				}
-			}
-			if (!lt_critica && !lt_urgente) {
-				//nivel normañ	
-				flag_lt_critica[t] = false;
-				flag_lt_urgente[t] = false;
-				$("#barra" + tq).css('background', 'black');
-				$("#nivel" + tq).css('color', 'green');
-				$("#nivel" + tq).css('background', 'black');
 			}
 			t++ //paso a la celda siguiente (tanque)
 		}
-
+		flag_alarma=!flag_alarma;
 	}
 }
 setInterval(mi_funcion, 1000);
