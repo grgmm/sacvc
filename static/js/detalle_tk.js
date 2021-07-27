cba0568922ca14c79ca8829fc7ee605fd3b0b6eb
jopegoco <jcotelo@gmail.com>
2021-07-27 13:09:12 +0200

    Construccion parcial detalle_tk 70%

--- static/js/detalle_tk.js
+++ static/js/detalle_tk.js
@@ -10,7 +10,7 @@ function mi_funcion() {
 	}).done(function (datos) {
 		if (JSON.stringify(datos) != JSON.stringify(datos_anterior)) {
 			idtk = document.getElementById("nrotanque").innerHTML;
-			console.log("miTanque "+idtk)
+			console.log("miTanque " + idtk)
 			//solo se ejecuta si el json cambia
 			actualizar_vista(datos);
 		}
@@ -32,13 +32,26 @@ function mi_funcion() {
 		nivel = document.getElementById("barra"); //nivel en UI
 		nivel.style.width = lt_porcentaje + "%"; //barra de progreso
 		$("#nivel").text(lt_porcentaje + "%");//Valor numerico del nivel
-		console.log("dato "+datos[idtk]["LT"]+" "+datos[idtk]["LT_UNIDAD"]);
-		$("#lt").text(datos[idtk]["LT"]+" "+datos[idtk]["LT_UNIDAD"]);
-		$("#tov").text(datos[idtk]["TOV"]+" "+datos[idtk]["TOV_UNIDAD"]);
-		$("#nsv").text(datos[idtk]["NSV"]+" "+datos[idtk]["NSV_UNIDAD"]);
-		$("#gsv").text(datos[idtk]["GSV"]+" "+datos[idtk]["GSV_UNIDAD"]);
-		
-		
+		//console.log("dato " + datos[idtk]["LT"] + " " + datos[idtk]["LT_UNIDAD"]);
+
+		//variables principales
+		$("#lt").text(datos[idtk]["LT"] + " " + datos[idtk]["LT_UNIDAD"]);
+		$("#tov").text(datos[idtk]["TOV"] + " " + datos[idtk]["TOV_UNIDAD"]);
+		$("#nsv").text(datos[idtk]["NSV"] + " " + datos[idtk]["NSV_UNIDAD"]);
+		$("#gsv").text(datos[idtk]["GSV"] + " " + datos[idtk]["GSV_UNIDAD"]);
+		//variables complementarias
+		$("#pt").text(datos[idtk]["PT"] + " " + datos[idtk]["LT_UNIDAD"]);
+		$("#ays").text(datos[idtk]["AYS"] + " " + datos[idtk]["TOV_UNIDAD"]);
+		$("#tt").text(datos[idtk]["TT"] + " " + datos[idtk]["NSV_UNIDAD"]);
+		$("#lta").text(datos[idtk]["LTA"] + " " + datos[idtk]["LT_UNIDAD"]);
+		//rangos de operacion
+		$("#lt_maximo").text(datos[idtk]["LT"] + " " + datos[idtk]["LT_UNIDAD"]);
+		$("#lt_minimo").text(datos[idtk]["TOV"] + " " + datos[idtk]["LT_UNIDAD"]);
+		$("#tov_maximo").text(datos[idtk]["NSV"] + " " + datos[idtk]["TOV_UNIDAD"]);
+		$("#tov_minimo").text(datos[idtk]["GSV"] + " " + datos[idtk]["TOV_UNIDAD"]);
+
+
+
 		datos_anterior = datos;//Evita actualizar datos innecesariamente
 		console.log("datos actualizados");
 	}
@@ -46,54 +59,268 @@ function mi_funcion() {
 	function manejar_alarmas(datos) {
 		//solo se ejecuta si el json cambia
 		//todo en unidades de ingenieria
-		var lt_urgente;
-		var lt_critica;
+		var lt_alarma, pt_alarma, tt_alarma, lta_alarma, ays_alarma, tov_alarma, nsv_alarma, gsv_alarma;
+		//recojo estado actual de las alarmas
+		lt_alarma = datos[idtk]["LT_ALARMA"];
+		pt_alarma = datos[idtk]["PT_ALARMA"];
+		tt_alarma = datos[idtk]["TT_ALARMA"];
+		lta_alarma = datos[idtk]["LTA_ALARMA"];
+		ays_alarma = datos[idtk]["AYS_ALARMA"];
+		tov_alarma = datos[idtk]["TOV_ALARMA"];
+		lt_porcentaje = datos[idtk]["LT_PORCENTAJE"];
+		nsv_alarma = datos[idtk]["NSV_ALARMA"];
+		gsv_alarma = datos[idtk]["GSV_ALARMA"];
+		lt_porcentaje = datos[idtk]["LT_PORCENTAJE"];
+		nivel = document.getElementById("barra");
 
-			lt_urgente = datos[idtk]["LT_URGENTE"];
-			lt_critica = datos[idtk]["LT_CRITICA"];
 
-			//Calculo de alarmas
-			if (lt_critica) {
-				$("#barra").css('background', 'red');//color de barra
-				$("#nivel").css('background', 'red');//color de fondo de la fuente
-				$("#nivel").css('color', 'black');//color de fuente
+		/*para el caso donde lt_porcentaje no contiene un numero;
+		Debemos definir un color en la barra y el fondo del valor*/
+
+		if (parseFloat(lt_porcentaje).toString() == "NaN") {
+			lt_porcentaje = "--.--"
+			nivel.style.width = "100" + "%"; //barra de progreso
+			$("#barra").css('background', 'gray');//color de barra
+			$("#nivel").css('background', 'gray');//color de fondo de la fuente
+			$("#nivel").css('color', 'black');
+		}
+		nivel.style.width = lt_porcentaje + "%"; //barra de progreso
+		$("#nivel").text(lt_porcentaje + "%");//Valor numerico del nivel
+
+		//ESTADO DE ALARMAS
+		//ALARMA LT
+		if (lt_alarma == "C") {
+			//barra de progreso y nivel
+			$("#barra").css('background', 'red');//color de barra
+			//$("#nivel").css('background', 'red');//color de fondo de la fuente
+			$("#nivel").css('color', 'black');//color de fuente
+
+			//Alarma en la tabla
+			$("#lt_alarma").css('background', 'red');
+
+			if (flag_alarma) {
+				$("#nivel").css('background', 'red');
+				$("#lt_alarma").css('background', 'red');//alarma en la tabla
+			} else {
+				//off
+				$("#nivel").css('background', 'none');
+				$("#lt_alarma").css('background', 'white');//alarma en la tabla
+			}
+		} else {
+			if (lt_alarma == "U") {
+				$("#barra").css('background', 'yellow');//color de barra
+				$("#nivel").css('background', 'yellow');//color de fondo de la fuente
+				$("#nivel").css('color', 'black');//color de fuente//Alarma en la tabla
+				$("#lt_alarma").css('background', 'yellow');
 				if (flag_alarma) {
-					//on
-					$("#nivel").css('background', 'red');
+					$("#nivel").css('background', 'yellow');
+					$("#lt_alarma").css('background', 'yellow');//alarma en la tabla
 				} else {
-					//off
 					$("#nivel").css('background', 'none');
+					$("#lt_alarma").css('background', 'white');//alarma en la tabla
 				}
 			} else {
-				if (lt_urgente) {
-					$("#barra").css('background', 'yellow');//color de barra
-					$("#nivel").css('background', 'yellow');//color de fondo de la fuente
-					$("#nivel").css('color', 'black');//color de fuente
-					if (flag_alarma) {
-						$("#nivel").css('background', 'yellow');
-					} else {
-						$("#nivel").css('background', 'none');
-					}
-				} else {
-					$("#barra").css('background', 'black');
-					$("#nivel").css('color', 'green');
-					$("#nivel").css('background', 'black');
-				}
+				//sin alarmas
+				$("#barra").css('background', 'black');
+				$("#nivel").css('color', 'green');
+				$("#nivel").css('background', 'black');
+				$("#lt_alarma").css('background', 'white');//alarma en la tabla
 			}
-			/*para el caso donde lt_porcentaje no contiene un numero;
-			Debemos definir un color en la barra y el fondo del valor*/
-			lt_porcentaje = datos[idtk]["LT_PORCENTAJE"];
-			nivel = document.getElementById("barra");
+		}
 
-			if (parseFloat(lt_porcentaje).toString() == "NaN") {
-				lt_porcentaje = "--.--"
-				nivel.style.width = "100" + "%"; //barra de progreso
-				$("#barra").css('background', 'gray');//color de barra
-				$("#nivel").css('background', 'gray');//color de fondo de la fuente
-				$("#nivel").css('color', 'black');
+		//ALARMA PT
+		if (pt_alarma == "C") {
+			//Alarma en la tabla
+			$("#pt_alarma").css('background', 'red');
+
+			if (flag_alarma) {
+				$("#pt_alarma").css('background', 'red');//alarma en la tabla
+			} else {
+				$("#pt_alarma").css('background', 'white');//alarma en la tabla
 			}
-			nivel.style.width = lt_porcentaje + "%"; //barra de progreso
-			$("#nivel").text(lt_porcentaje + "%");//Valor numerico del nivel
+		} else {
+			if (pt_alarma == "U") {
+				$("#pt_alarma").css('background', 'yellow');
+				if (flag_alarma) {
+					$("#pt_alarma").css('background', 'yellow');//alarma en la tabla
+				} else {
+					$("#pt_alarma").css('background', 'white');//alarma en la tabla
+				}
+			} else {
+				//sin alarmas
+				$("#pt_alarma").css('background', 'white');//alarma en la tabla
+			}
+		}
+
+		//ALARMA TT
+		if (tt_alarma == "C") {
+			//Alarma en la tabla
+			$("#tt_alarma").css('background', 'red');
+
+			if (flag_alarma) {
+				$("#tt_alarma").css('background', 'red');//alarma en la tabla
+			} else {
+				$("#tt_alarma").css('background', 'white');//alarma en la tabla
+			}
+		} else {
+			if (tt_alarma == "U") {
+				$("#tt_alarma").css('background', 'yellow');
+				if (flag_alarma) {
+					$("#tt_alarma").css('background', 'yellow');//alarma en la tabla
+				} else {
+					$("#tt_alarma").css('background', 'white');//alarma en la tabla
+				}
+			} else {
+				//sin alarmas
+				$("#tt_alarma").css('background', 'white');//alarma en la tabla
+			}
+		}
+
+		//ALARMA LTA
+		if (lta_alarma == "C") {
+			//Alarma en la tabla
+			$("#lta_alarma").css('background', 'red');
+
+			if (flag_alarma) {
+				$("#lta_alarma").css('background', 'red');//alarma en la tabla
+			} else {
+				$("#lta_alarma").css('background', 'white');//alarma en la tabla
+			}
+		} else {
+			if (lta_alarma == "U") {
+				$("#lta_alarma").css('background', 'yellow');
+				if (flag_alarma) {
+					$("#lta_alarma").css('background', 'yellow');//alarma en la tabla
+				} else {
+					$("#lta_alarma").css('background', 'white');//alarma en la tabla
+				}
+			} else {
+				//sin alarmas
+				$("#lta_alarma").css('background', 'white');//alarma en la tabla
+			}
+		}
+
+		//ALARMA AYS
+		if (ays_alarma == "C") {
+			//Alarma en la tabla
+			$("#ays_alarma").css('background', 'red');
+
+			if (flag_alarma) {
+				$("#ays_alarma").css('background', 'red');//alarma en la tabla
+			} else {
+				$("#ays_alarma").css('background', 'white');//alarma en la tabla
+			}
+		} else {
+			if (ays_alarma == "U") {
+				$("#ays_alarma").css('background', 'yellow');
+				if (flag_alarma) {
+					$("#ays_alarma").css('background', 'yellow');//alarma en la tabla
+				} else {
+					$("#ays_alarma").css('background', 'white');//alarma en la tabla
+				}
+			} else {
+				//sin alarmas
+				$("#ays_alarma").css('background', 'white');//alarma en la tabla
+			}
+		}
+
+		//ALARMA TOV
+		if (tov_alarma == "C") {
+			//Alarma en la tabla
+			$("#tov_alarma").css('background', 'red');
+
+			if (flag_alarma) {
+				$("#tov_alarma").css('background', 'red');//alarma en la tabla
+			} else {
+				$("#tov_alarma").css('background', 'white');//alarma en la tabla
+			}
+		} else {
+			if (tov_alarma == "U") {
+				$("#tov_alarma").css('background', 'yellow');
+				if (flag_alarma) {
+					$("#tov_alarma").css('background', 'yellow');//alarma en la tabla
+				} else {
+					$("#tov_alarma").css('background', 'white');//alarma en la tabla
+				}
+			} else {
+				//sin alarmas
+				$("#tov_alarma").css('background', 'white');//alarma en la tabla
+			}
+		}
+
+		//ALARMA NSV
+		if (nsv_alarma == "C") {
+			//Alarma en la tabla
+			$("#nsv_alarma").css('background', 'red');
+
+			if (flag_alarma) {
+				$("#nsv_alarma").css('background', 'red');//alarma en la tabla
+			} else {
+				$("#nsv_alarma").css('background', 'white');//alarma en la tabla
+			}
+		} else {
+			if (nsv_alarma == "U") {
+				$("#nsv_alarma").css('background', 'yellow');
+				if (flag_alarma) {
+					$("#nsv_alarma").css('background', 'yellow');//alarma en la tabla
+				} else {
+					$("#nsv_alarma").css('background', 'white');//alarma en la tabla
+				}
+			} else {
+				//sin alarmas
+				$("#nsv_alarma").css('background', 'white');//alarma en la tabla
+			}
+		}
+
+		//ALARMA NSV
+		if (nsv_alarma == "C") {
+			//Alarma en la tabla
+			$("#nsv_alarma").css('background', 'red');
+
+			if (flag_alarma) {
+				$("#nsv_alarma").css('background', 'red');//alarma en la tabla
+			} else {
+				$("#nsv_alarma").css('background', 'white');//alarma en la tabla
+			}
+		} else {
+			if (nsv_alarma == "U") {
+				$("#nsv_alarma").css('background', 'yellow');
+				if (flag_alarma) {
+					$("#nsv_alarma").css('background', 'yellow');//alarma en la tabla
+				} else {
+					$("#nsv_alarma").css('background', 'white');//alarma en la tabla
+				}
+			} else {
+				//sin alarmas
+				$("#nsv_alarma").css('background', 'white');//alarma en la tabla
+			}
+		}
+
+		console.log("Alarma "+gsv_alarma)
+		//ALARMA GSV
+		if (gsv_alarma == "C") {
+			//Alarma en la tabla
+			console.log("Alarma "+gsv_alarma)
+			$("#gsv_alarma").css('background', 'red');
+
+			if (flag_alarma) {
+				$("#gsv_alarma").css('background', 'red');//alarma en la tabla
+			} else {
+				$("#gsv_alarma").css('background', 'white');//alarma en la tabla
+			}
+		} else {
+			if (gsv_alarma == "U") {
+				$("#gsv_alarma").css('background', 'yellow');
+				if (flag_alarma) {
+					$("#gsv_alarma").css('background', 'yellow');//alarma en la tabla
+				} else {
+					$("#gsv_alarma").css('background', 'white');//alarma en la tabla
+				}
+			} else {
+				//sin alarmas
+				$("#gsv_alarma").css('background', 'white');//alarma en la tabla
+			}
+		}
 
 		flag_alarma = !flag_alarma;
 	}
