7c55463337e42d98434882aaa60bca4db6377187
jopegoco <jcotelo@gmail.com>
2021-09-03 14:05:51 +0200

    Mejoras en MbMaestro.html, se mejora formato

--- static/js/mb_maestro1.js
+++ static/js/mb_maestro1.js
@@ -6,8 +6,7 @@ var e_serviceport = document.getElementById("id_SercvicePort");    //input
 var e_velocidad = document.getElementById("id_Velocidad");       //select
 var e_paridad = document.getElementById("id_Paridad");         //select
 var e_reintentos = document.getElementById("id_Reintentos");      //input
-var e_iddevice = document.getElementById("id_IdDevice");        //input
-console.log(e_tipo);
+var e_iddevice = document.getElementById("id_IdDevice"); 
 function show() {
     //obtengo el string contenido en la opcion seleccionada
     var text_id_tipo = e_tipo.options[e_tipo.selectedIndex].text;
@@ -43,12 +42,14 @@ function show() {
 
         //parametros no usados ocultos
         //e_ipdevice.value = "000 000 000 000";
-        e_ipdevice.style.color = "white";
+        //e_ipdevice.style.color = "white"; //temporal
+        e_ipdevice.style.visibility = "hidden"; 
         //e_serviceport.value = "0000";
-        e_serviceport.style.color = "white";
+        //e_serviceport.style.color = "white"; //temporal
+        e_serviceport.style.visibility = "hidden"; 
 
         //Deshabilito los input y habilito el id="id_Reintentos"
-        $('input').prop('disabled', true);
+        //$('input').prop('disabled', true);
         e_reintentos.disabled = false;
         //habilito el input con id=id_IdDevice
         e_iddevice.disabled = false;
@@ -58,8 +59,10 @@ function show() {
         //habilito input
         $('input').prop('disabled', false);
         //parametros TCP visibles
-        e_ipdevice.style.color = "black";
-        e_serviceport.style.color = "black";
+        //e_ipdevice.style.color = "black";
+        e_ipdevice.style.visibility = "visible"; 
+        //e_serviceport.style.color = "black";
+        e_serviceport.style.visibility = "visible"; 
 
         //Retiro las opciones seriales
         e_puerto.value = 'serial1';
@@ -83,8 +86,9 @@ function show() {
         e_puerto.options[e_puerto.selectedIndex].style.display = "inline";
 
         //Retiro visibilidad a select paridad y velocidad
-        e_velocidad.style.display = 'none';
-        e_paridad.style.display = 'none';
+        e_velocidad.style.display = 'inline';
+        e_paridad.style.display = 'inline';
+        
     }
     //Habilito el boton de seleccion de archivo del dialog
     document.getElementById("id_Configuracion").disabled = false;
